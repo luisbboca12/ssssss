3fdea37748e3546d535e07070706985b7b74bb05
luisbboca12 <107447827+luisbboca12@users.noreply.github.com>
2023-08-30 14:34:09 -0300

    Update mediana.js

--- mediana.js
+++ mediana.js
@@ -2,7 +2,7 @@
 function esPar(array) {
     let arrayCantidadElementos = array.length
     if ((arrayCantidadElementos % 2)===0) {
-        true
+        return true
         
     } else {
         return false
@@ -14,7 +14,13 @@ function esPar(array) {
 function calcularMediana(array) {
     let esLaListaPar = esPar(array)
     if (esLaListaPar) {
-        //
+        let index1Mediana = Math.floor(array.length/2)-1
+        let index2Mediana = Math.floor(array.length/2)
+        let primerMediana= array[index1Mediana]
+        let segundaMediana= array[index2Mediana]
+        let medianaPar = (primerMediana+ segundaMediana)/2
+        console.log(medianaPar)
+        return medianaPar
         
     } else {
         let indexMedianaImpar = Math.floor(array.length/2)
@@ -25,4 +31,4 @@ function calcularMediana(array) {
     }
     
 }
-calcularMediana([12,3,4])
+calcularMediana([12,6,4,4])
